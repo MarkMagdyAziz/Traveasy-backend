59f256856188f226869edcaa649f49549a29a6a3
Mark M Aziz <mark.aziz.magdy@gmail.com>
2022-11-01 12:07:39 +0200

    front end post

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,27 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
+  username: {
+    type: String,
+    required: [true, 'username name is required!!'],
+    trim: true,
+    unique: true,
+    minLength: 3,
+    maxLength: 25,
+  },
+  email: {
+    type: String,
+    required: [true, 'email name is required!!'],
+    unique: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: [true, 'password name is required!!'],
+    minLength: 6,
+    maxLength: 20,
+  },
   roles: [
     {
       type: mongoose.Schema.Types.ObjectId,
